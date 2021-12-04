79ec9a8f67da5c9db0d88c63543cd44bdf29e028
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-12-04 23:16:18 +0200

    add | Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsModule } from './posts/posts.module';
 import { HeaderComponent } from './modules/shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent],
-    imports: [BrowserModule, AppRoutingModule, PostsModule],
+    imports: [BrowserModule, AppRoutingModule, PostsModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
