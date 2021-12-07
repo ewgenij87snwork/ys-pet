f65e00d3022ae213e91dcdba28df2e228cede7e8
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-12-08 00:02:06 +0200

    add | Validations messages by ErrorStateMatcher & <mat-error>

--- src/app/modules/shared/components/form-input/form-input.module.ts
+++ src/app/modules/shared/components/form-input/form-input.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormInputComponent } from './form-input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     declarations: [FormInputComponent],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
     exports: [FormInputComponent],
 })
 export class FormInputModule {}
